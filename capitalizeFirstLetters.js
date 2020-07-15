b62b4f8754fe54354bdac3f6a9caa366a93eadd6
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-07-15 08:11:42 +0200

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,12 @@
-function capitalizeFirstLetters(input) {}
-
 const assert = require("assert");
+const capitalizeFirstLetters = (input) => {
+  input = input.split(" ");
+  for (let i = 0; i < input.length; i++) {
+    input[i] = input[i].charAt(0).toUpperCase() + input[i].substr(1);
+  }
+  return input.join(" ");
+};
+
 assert.strictEqual(
   capitalizeFirstLetters("i am learning TDD"),
   "I Am Learning TDD"
